1ca7b39756d19537e8022f49fef65b3e01b48e05
Tim Arterbury <timarterbury@gmail.com>
2019-11-15 15:58:23 -0600

    Updates tables to correct postgres syntax and adds stored procedures. Sucessfully imports and works in postgres

--- database/mapzest-postgresql-db-init.sql
+++ database/mapzest-postgresql-db-init.sql
@@ -1,10 +1,11 @@
 --  ============================================================================
 --  MapZest PostgreSQL Database Initialization
 --
---  Runs MySQL scripts for database creation, procedure creation, and default
---  insertions.
+--  Runs MySQL scripts for database creation, procedure creation, and any
+--  default insertions.
 --  ============================================================================
 
-SOURCE mapzest-postgresql-db-init_tables.sql;
-SOURCE mapzest-postgresql-db-init_procedures.sql;
--- SOURCE mapzest-postgresql-db-init_insertions.sql;
+-- Include Table and Procedure scripts.
+\i mapzest-postgresql-db-init_tables.sql
+\i mapzest-postgresql-db-init_procedures.sql;
+-- \i mapzest-postgresql-db-init_insertions.sql;
--- database/mapzest-postgresql-db-init_tables.sql
+++ database/mapzest-postgresql-db-init_tables.sql
@@ -4,100 +4,92 @@
 
 /* Create Database *************************************************************
 *******************************************************************************/
-CREATE DATABASE MapZest
-    WITH USER 'postgres'
-    ENCODING 'UTF8'
-    LC_COLLATE 'en_US.UTF-8'
-    LC_CTYPE 'en_US.UTF-8';
+CREATE DATABASE mapzest ENCODING 'UTF8';
+    -- LC_COLLATE 'en_US.UTF-8'
+    -- LC_CTYPE 'en_US.UTF-8';
 
 
 -- Connect to MapZest DB
-\c MapZest
+\c mapzest
 
 /* Create Tables ***************************************************************
 *******************************************************************************/
 
-CREATE TABLE Users (
-    userId INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE users (
+    user_id SERIAL PRIMARY KEY,
     email VARCHAR (345) UNIQUE NOT NULL, -- Unique is automatically indexed
     password VARCHAR (255) NOT NULL, -- Salt + Hash of user's password
-    mostRecentLocationId INT UNSIGNED DEFAULT NULL,
-
     -- User must verify their email before using their account
     -- This is NULL when the user is not verified and a timestamp when they are
-    emailVerifiedAt TIMESTAMP WITH TIME ZONE DEFAULT NULL,
-
-    createdAt TIMESTAMP WITH TIME ZONE NOT NULL,
-
-    -- Are foreign Key Refs are automatically indexed by Postgresql?
-    FOREIGN KEY (mostRecentLocationId) REFERENCES Locations (locationId)
-        ON UPDATE CASCADE
-        ON DELETE RESTRICT
+    email_verification_code BYTEA NOT NULL, -- 64 Bytes
+    email_verified_at TIMESTAMP WITH TIME ZONE DEFAULT NULL,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL
 );
 
-/*  UserEmailVerificationCodes
 
-    A table of temporary email verification codes that are sent to User's
-    via email allowing them to click a link to verify their User account.
-*/
-CREATE TABLE UserEmailVerificationCodes (
-    userId INT UNSIGNED UNIQUE NOT NULL,
-    emailVerificationCode VARCHAR (64) NOT NULL,
+CREATE TABLE user_login_tokens (
+    user_id INTEGER UNIQUE NOT NULL,
+    password_reset_code BYTEA NOT NULL, -- 64 Bytes
+    requested_at TIMESTAMP WITH TIME ZONE NOT NULL,
 
-    FOREIGN KEY (userId) REFERENCES Users (userId)
+    FOREIGN KEY (user_id) REFERENCES users (user_id)
         ON UPDATE CASCADE
         ON DELETE CASCADE
 );
 
 
-/*  UserPasswordResetCodes
+/*  user_password_reset_codes
 
     A table of temporary password reset codes that are sent to User's
     via email allowing them to reset their password.
 
-    I'm storing the requestedAt as opposed to an expiration time since
+    I'm storing the requested_at as opposed to an expiration time since
     this makes it easy for my application to decide how long until a code
     expires instead of the database itself.
 */
-CREATE TABLE UserPasswordResetCodes (
-    userId INT UNSIGNED UNIQUE NOT NULL,
-    passwordResetCode VARCHAR (64) NOT NULL,
-    requestedAt TIMESTAMP WITH TIME ZONE NOT NULL,
+CREATE TABLE user_password_reset_codes (
+    user_id INTEGER UNIQUE NOT NULL,
+    password_reset_code BYTEA NOT NULL, -- 64 Bytes
+    requested_at TIMESTAMP WITH TIME ZONE NOT NULL,
 
-    FOREIGN KEY (userId) REFERENCES Users (userId)
+    FOREIGN KEY (user_id) REFERENCES users (user_id)
         ON UPDATE CASCADE
         ON DELETE CASCADE
 );
 
 
-CREATE TABLE Locations (
-    locationId INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    userId INT UNSIGNED NOT NULL,
+CREATE TABLE locations (
+    location_id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL,
     latitude REAL NOT NULL,
     longitude REAL NOT NULL,
-    createdAt TIMESTAMP WITH TIME ZONE NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL,
 
     -- Are foreign Key Refs are automatically indexed by Postgresql?
-    FOREIGN KEY (userId) REFERENCES Users (userId)
+    FOREIGN KEY (user_id) REFERENCES users (user_id)
         ON UPDATE CASCADE
         ON DELETE CASCADE
 );
 
 
-CREATE TYPE FRIEND_STATUS AS ENUM ('unspecified', 'rejected', 'accepted');
+CREATE TYPE friend_status AS ENUM ('unspecified', 'rejected', 'accepted');
 
-CREATE TABLE Friends (
-    user1Id INT UNSIGNED UNIQUE NOT NULL,
-    user2Id INT UNSIGNED UNIQUE NOT NULL,
-    user1Status FRIEND_STATUS NOT NULL DEFAULT 'unspecified',
-    user2Status FRIEND_STATUS NOT NULL DEFAULT 'unspecified',
-    lastUpdate TIMESTAMP WITH TIME ZONE NOT NULL,
+CREATE TABLE friends (
+    -- The lower user id of two users will always be user_1,
+    -- the higher user id of the two will always be user_2.
+    user_1_id INTEGER NOT NULL,
+    user_2_id INTEGER NOT NULL,
+    user_1_status friend_status NOT NULL DEFAULT 'unspecified',
+    user_2_status friend_status NOT NULL DEFAULT 'unspecified',
+    updated_at TIMESTAMP WITH TIME ZONE NOT NULL,
+
+    PRIMARY KEY (user_1_id, user_2_id),
 
     -- Are foreign Key Refs are automatically indexed by Postgresql?
-    FOREIGN KEY (user1Id) REFERENCES Users (userId)
+    FOREIGN KEY (user_1_id) REFERENCES users (user_id)
         ON UPDATE CASCADE
         ON DELETE CASCADE,
-    FOREIGN KEY (user2Id) REFERENCES Users (userId)
+    FOREIGN KEY (user_2_id) REFERENCES users (user_id)
         ON UPDATE CASCADE
         ON DELETE CASCADE
 );
